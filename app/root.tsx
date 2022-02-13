4e430ece3d42f3b15812064d8836a2dce49bf6eb
Junior Martins <amjr.box@gmail.com>
2022-02-13 16:52:05 -0300

    Up root

--- app/root.tsx
+++ app/root.tsx
@@ -8,12 +8,15 @@ import {
   Link,
 } from 'remix'
 import type { MetaFunction } from 'remix'
+import invariant from 'tiny-invariant'
 
 export const meta: MetaFunction = () => {
   return { title: 'Learning Remix' }
 }
 
 export default function App() {
+  invariant(false, '👋 era esperado uma rota aqui')
+
   return (
     <html lang='pt-br'>
       <head>
@@ -26,7 +29,7 @@ export default function App() {
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        <Link to='/'>Home</Link> | <Link to='/posts'>Posts</Link> |{' '}
+        <Link to='/'>Home</Link>| <Link to='/posts'>Posts</Link> |{' '}
         <Link to='/admin'>Admin</Link>
         {process.env.NODE_ENV === 'development' && <LiveReload />}
       </body>
